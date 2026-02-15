2ad26cf7e6d5cda76ccb5668ff814e49771daa8b
DatNgyM <nguyenminhdat03112003@gmail.com>
2026-02-15 11:19:05 +0700

    chore: update next config and dependencies

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cf.shopee.vn', pathname: '/**' },
+      { protocol: 'https', hostname: 'shopee.vn', pathname: '/**' },
+    ],
+  },
+}
 
 module.exports = nextConfig
 
